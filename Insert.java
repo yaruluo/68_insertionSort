6cb7df81d44389883ce8e07d2086494c56525427
Yaru Luo <yluo00@stuy.edu>
2019-05-05 19:56:01 -0400

    im stupid

--- Insert.java
+++ Insert.java
@@ -1,43 +1,59 @@
+import java.util.ArrayList;
+
 public class Insert {
 
-    public void insert1( List_inArraySlots list, int alreadyInserted){
-	String valueToInsert = list[ alreadyInserted];
+    public static void insert1( List_inArraySlots list, int alreadyInserted){
+	int valueToInsert = list.get( alreadyInserted);
 
-	if( list[ 0] > valueToInsert){
-	    //shift 
-	    for( int index = alreadyInserted; index != 0; index--)
-		list[ index] = list[ index-1];
-            //drop
-	    list[ 0] = valueToInsert;
+	if( list.get( 0) < valueToInsert){
+	    shift( list, 0, alreadyInserted);
+            drop( list, 0, valueToInsert);
 	}
 
 	else{
-	    for( int indexToCompare = alreadyInserted
-		     ; indexToCompare != 0
+	    for( int indexToCompare = alreadyInserted-1
+		     ; indexToCompare != -1
 		     ; indexToCompare--){
-		if( valueToInsert < list[ indexToCompare]){
-		    //shift 
-		    for( int index = alreadyInserted; index != 0; index--)
-			list[ index] = list[ index-1];
-		}
-		else
-		    //drop
-		    list[ indexToCompare] = valueToInsert;
+		if( valueToInsert < list.get( indexToCompare)){
+		    shift( list, indexToCompare, alreadyInserted);
+		  	}
+		 else 
+		    drop( list, indexToCompare, valueToInsert);
 	    }
 	}
     }
 
-    public void insertionSort( List_inArraySlots list){
-	for( int goodNameNeeded = 1
-		 ; goodNameNeeded < list.size()
-		 ; goodNameNeeded++){
-	    insert1( list, goodNameNeeded);
+    public static void shift( List_inArraySlots list, int begin, int end){
+	for( int index = end; index != begin; index--)
+	    list.get( index) = list.get( index-1);
+    }
+
+    public static void drop( List_inArraySlots list, int index, int value){
+	list.set( index, value);
+    }
+
+    public static void insertionSort( List_inArraySlots list){
+	for( int boundary = 1
+		 ; boundary < list.size()
+		 ; boundary++){
+	    insert1( list, boundary);
 	    // for debugging
 	    System.out.println( "    dbg: "
-				+ "after inserting element " + goodNameNeeded
+				+ "after inserting element " + boundary
 				+ " elements: " + list
 				);
 	}
+    }
+
+    public static void main( String[] commandLine){
+	List_inArraySlots list = new List_inArraySlots();
+	list.add( 2);
+	list.add( 17);
+	list.add( 8);
+
+	System.out.println( "    initial: " + list);
+	insertionSort( list);
+	System.out.println( "    sorted: " + list);
     }
     
 }
