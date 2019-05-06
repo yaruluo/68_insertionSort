5c0ef41d08c7574da16bca8092e35b9ba59428f7
Yaru Luo <yluo00@stuy.edu>
2019-05-05 20:41:29 -0400

    aksjfaskj

--- Insert.java
+++ Insert.java
@@ -5,27 +5,29 @@ public class Insert {
     public static void insert1( List_inArraySlots list, int alreadyInserted){
 	int valueToInsert = list.get( alreadyInserted);
 
-	if( list.get( 0) < valueToInsert){
+	if( list.get( 0) > valueToInsert){
 	    shift( list, 0, alreadyInserted);
             drop( list, 0, valueToInsert);
 	}
-
 	else{
 	    for( int indexToCompare = alreadyInserted-1
 		     ; indexToCompare != -1
 		     ; indexToCompare--){
 		if( valueToInsert < list.get( indexToCompare)){
-		    shift( list, indexToCompare, alreadyInserted);
+		    shift( list, indexToCompare, indexToCompare+1);
 		  	}
-		 else 
+		else 
 		    drop( list, indexToCompare, valueToInsert);
+	        
 	    }
 	}
     }
 
     public static void shift( List_inArraySlots list, int begin, int end){
-	for( int index = end; index != begin; index--)
-	    list.get( index) = list.get( index-1);
+	for( int index = end; index != begin; index--){
+	    list.set( index, list.get( index-1));
+	}
+	//	list.set( begin, list.set( begin+1, list.get( begin)));
     }
 
     public static void drop( List_inArraySlots list, int index, int value){
@@ -47,9 +49,10 @@ public class Insert {
 
     public static void main( String[] commandLine){
 	List_inArraySlots list = new List_inArraySlots();
-	list.add( 2);
-	list.add( 17);
+	list.add( 12);
+	list.add( 11);
 	list.add( 8);
+	list.add(1);
 
 	System.out.println( "    initial: " + list);
 	insertionSort( list);
